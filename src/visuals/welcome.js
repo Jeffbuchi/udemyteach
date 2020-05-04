12c30172c8df6213f20c7f9bd2684b1d401abe74
Jeffbuchi <buchisam32@gmail.com>
2020-05-05 00:37:57 +0100

    working on component

--- src/visuals/welcome.js
+++ src/visuals/welcome.js
@@ -1,36 +1,37 @@
 
-
-import React, {useEffect,useState} from "react";
+import React, { useEffect,useState } from "react";
 import ReactDOM from "react-dom";
 import GoogleLogin from "react-google-login";
 
 function WelcomePage() {
+
+    useEffect(() => {
+        document.title = "Welcome"
+    })
+    const [login, setLogin] = useState(false);
     const [name, setName] = useState("");
     const [email, setEmail] = useState("");
     const [url, setUrl] = useState("");
-    
-
-  
     const responseGoogle = response => {
        setName(response.profileObj.name);
        setEmail(response.profileObj.email);
        setUrl(response.profileObj.imageUrl);
-      
+       setLogin(true);
     };
     
-   
+    console.log(login)
     return (
         <div className = "welcome">
-            <h3 className = "text-logon">{name}</h3>
+            <h3 className = "text-logon">{ !login ? "Welcome! Login here.": name }</h3>
             <br/>
             <img className = "img-login" src = {url} alt = {name}/>
-           <GoogleLogin className = "logon"
+            { !login ? <GoogleLogin className = "logon"
     clientId="35041798621-lu7bdn5b402mhcgmq9g8o78h2crf6v85.apps.googleusercontent.com"
     buttonText="Login"
     onSuccess={responseGoogle}
     onFailure={responseGoogle}
     cookiePolicy={'single_host_origin'}
-    />
+    />: ""}
         </div>
     
 );
